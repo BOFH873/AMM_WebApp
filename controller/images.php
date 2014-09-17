6ae8cbb6ee02be46391dc2dec339af3324a2b6c7
BOFH873 <drgbgt@gmail.com>
2014-09-17 17:00:07 +0200

    sostituzione include con require

--- controller/images.php
+++ controller/images.php
@@ -18,8 +18,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-include_once __DIR__."/../Database.php";
-include_once __DIR__."/../model/Product.php";
+require_once __DIR__."/../Database.php";
+require_once __DIR__."/../model/Product.php";
 
 switch ($_REQUEST["type"]) {
 
