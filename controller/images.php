48224bdf78cb1d594885ed76e87be7ce5d285894
BOFH873 <drgbgt@gmail.com>
2014-09-17 18:25:45 +0200

    Aggiunta header per memorizzazione immagini in cache

--- controller/images.php
+++ controller/images.php
@@ -33,7 +33,10 @@ switch ($_REQUEST["type"]) {
             exit();
         }
         
-        $pic = $result->getPicture();
+        header('Pragma: public');
+        header('Cache-Control: max-age=86400');
+        header('Expires: '. gmdate('D, d M Y H:i:s \G\M\T', time() + 86400));
+        header('Content-Type: image/png');        $pic = $result->getPicture();
 
         if ($pic != null)
         {
